8129d5be3931a5f18d130bd36f265fd2b1deca71
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-04-25 19:07:07 +0200

    First commit, project beginning

---
+++ functional_tests.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+import time
+
+class anonymous_visits_website(unittest.TestCase):
+	"""docstring for """
+	def __init__(self, arg):
+		super(, self).__init__()
+		self.arg = arg
