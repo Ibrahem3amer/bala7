69b0c1391cc3197124b2d3f4aa4b19b565ac062e
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-05-29 15:02:56 +0200

    Adding test file for APIs

---
+++ users/tests/test_apis.py
@@ -0,0 +1,11 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+class UsersAPITest(APITestCase):
+	def test_return_list_of_users(self):
+		# Setup test
+		
+		# Exercise test
+		# Assert test
+		
