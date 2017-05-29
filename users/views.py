85d531f2dbbfb1899fc85444bb4eeee96dfb6e3d
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-05-29 15:16:51 +0200

    Adding education form connections

--- users/views.py
+++ users/views.py
@@ -100,6 +100,7 @@ def update_user_education_info(request):
 			new_info['new_university_id'] 	= request.POST['new_university_id']
 			new_info['new_faculty_id'] 		= request.POST['new_faculty_id']
 			new_info['new_department_id'] 	= request.POST['new_department_id']
+			new_info['new_section_number'] 	= request.POST['new_section_number']
 		except AttributeError:
 			msg = 'University, faculty and department cannot be empty.'
 			return render(request, 'profile/profile.html', {'error':msg})
