0d0d1cd838580cc2c7a3b4b5194579ff84bebfc6
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-08-17 18:49:12 +0200

    Edit urls.py

--- bala7/urls.py
+++ bala7/urls.py
@@ -18,8 +18,7 @@ from django.conf.urls import url, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from users import views, urls
-from cms import urls
+from users import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
