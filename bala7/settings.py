63413184a0cfb0f5a83170ac3c9f6d555aa9c17d
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-08-19 19:48:37 +0200

    STATIC_ROOT added.

--- bala7/settings.py
+++ bala7/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/najiba/static'
 
 # Media files
 ENV_PATH    = os.path.abspath(os.path.dirname(__file__))
