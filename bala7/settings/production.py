07850b99d8b8d9986c1fbe4f8ee837240c7d6513
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-08-25 15:13:38 +0200

    Updating production security warnings.

--- bala7/settings/production.py
+++ bala7/settings/production.py
@@ -5,6 +5,18 @@ DEBUG = False
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+SECURE_CONTENT_TYPE_NOSNIFF = True
+
+SECURE_BROWSER_XSS_FILTER = True
+
+SESSION_COOKIE_SECURE = True
+
+CSRF_COOKIE_SECURE = True
+
+CSRF_COOKIE_HTTPONLY = True
+
+X_FRAME_OPTIONS = 'DENY'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
