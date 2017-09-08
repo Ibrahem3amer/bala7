10d0cec3bc96ba690759b4921679f280a9b8434c
ahmed-Elbana <ahmedelpna@gmail.com>
2017-09-08 02:23:57 +0200

    update tasks section

--- users/static/js/home.js
+++ users/static/js/home.js
@@ -1,18 +1,17 @@
-$(document).ready(function () {
-
-    $('.btn-vertical-slider').on('click', function () {
-        
-        if ($(this).attr('data-slide') == 'next') {
-            $('#myCarousel').carousel('next');
-        }
-        if ($(this).attr('data-slide') == 'prev') {
-            $('#myCarousel').carousel('prev')
-        }
-        if ($(this).attr('data-slide') == 'next') {
-            $('.myCarousel1').carousel('next');
-        }
-        if ($(this).attr('data-slide') == 'prev') {
-            $('.myCarousel1').carousel('prev')
+$(document).ready(function(){
+    
+    $('.owl-carousel').owlCarousel({
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1100:{
+                items:4
+            }
         }
     });
+    
 });
