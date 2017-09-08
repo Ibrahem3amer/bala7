32f8958a0c46bbf55ae4262f72349bab63860907
ibrahem3amer <ibrahem3amer@hotmail.com>
2017-09-08 23:16:21 +0200

    Removing url import.

--- cms/urls.py
+++ cms/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from cms import views, urls
+from cms import views
 
 urlpatterns = [
     url(r'^(?P<dep_id>[0-9]+)/(?P<topic_id>[0-9]+)$', views.get_topic, name = 'get_topic'),
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-from users import views, urls
+from users import views
 from cms.views import user_table
 
 urlpatterns = [
