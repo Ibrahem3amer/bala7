a2e14734dba2b87b8aaa49929282699d7a998b3e
ibrahem3amer <you@example.com>
2018-03-10 13:01:41 +0200

    Headmaster in API serializers fix.

--- users/serializers.py
+++ users/serializers.py
@@ -3,74 +3,71 @@ from users.models import *
 from cms.models import UserComment
 from django.contrib.auth.models import User
 
+
 class UserSerializer(serializers.ModelSerializer):
-	class Meta:
-		model 	= User
-		fields 	= ('id', 'username', 'email', 'password')
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'password')
+
 
 class UserProfileSerialzer(serializers.ModelSerializer):
-	class Meta:
-		model 	= UserProfile
-		fields 	= ('university', 'faculty', 'department', 'level', 'gender', 'count_of_posts', 'count_of_replies', 'topics')
+    class Meta:
+        model = UserProfile
+        fields = (
+        'university', 'faculty', 'department', 'level', 'gender', 'count_of_posts', 'count_of_replies', 'topics')
 
 
 class TeamSerializer(serializers.ModelSerializer):
-	
-	class Meta:
-		model = SVProfile
-		fields = ('__all__')
+    class Meta:
+        model = SVProfile
+        fields = ('__all__')
 
 
 class DepratmentSerializer(serializers.ModelSerializer):
-	
-	team = TeamSerializer(read_only=True)
-	
-	class Meta:
-		model 	= Department
-		fields 	= ('id', 'name', 'bio', 'team')
+    team = TeamSerializer(read_only=True)
+
+    class Meta:
+        model = Department
+        fields = ('id', 'name', 'bio', 'team')
 
 
 class FacultySerializer(serializers.ModelSerializer):
-
-	class Meta:
-		model 	= Faculty
-		fields 	= ('id', 'name', 'bio', 'headmaster', 'university')
+    class Meta:
+        model = Faculty
+        fields = ('id', 'name', 'bio', 'university')
 
 
 class UniversitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = University
+        fields = ('id', 'name', 'bio')
 
-	class Meta:
-		model 	= University
-		fields 	= ('id', 'name', 'bio', 'headmaster')
 
 class DepratmentLinkedSerializer(serializers.ModelSerializer):
-	class Meta:
-		model 	= Department
-		fields 	= ('id', 'name', 'bio', 'headmaster')
+    class Meta:
+        model = Department
+        fields = ('id', 'name', 'bio')
 
 
 class FacultyLinkedSerializer(serializers.ModelSerializer):
+    departments = DepratmentLinkedSerializer(many=True)
 
-	departments = DepratmentLinkedSerializer(many = True)
-
-	class Meta:
-		model 	= Faculty
-		fields 	= ('id', 'name', 'bio', 'headmaster', 'university', 'departments')
+    class Meta:
+        model = Faculty
+        fields = ('id', 'name', 'bio', 'university', 'departments')
 
 
 class UniversityLinkedSerializer(serializers.ModelSerializer):
+    faculties = FacultyLinkedSerializer(many=True)
 
-	faculties 	= FacultyLinkedSerializer(many = True)
+    class Meta:
+        model = University
+        fields = ('name', 'bio', 'faculties')
 
-	class Meta:
-		model 	= University
-		fields 	= ('name', 'bio', 'headmaster', 'faculties')
 
 class CommentSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
 
-	user = UserSerializer(read_only=True)
-
-	class Meta:
-		model = UserComment
-		fields = ('__all__')
-
+    class Meta:
+        model = UserComment
+        fields = ('__all__')
