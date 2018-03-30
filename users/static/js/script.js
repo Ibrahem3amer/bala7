70638c121ea85ff0e6a650c5f2641b0b3b04d6d0
ibrahem3amer <you@example.com>
2018-03-30 22:56:48 +0200

    About page was added.

--- users/static/js/script.js
+++ users/static/js/script.js
@@ -37,3 +37,27 @@ $(document).ready(function(){
       }
 
 });
+
+$(function() {
+    $("a").linkpreview({
+        previewContainer: "#preview-container"
+    });
+    $("input").linkpreview({
+        previewContainer: "#preview-container2",
+        refreshButton: "#refresh-button",
+        previewContainerClass: "row-fluid",
+        errorMessage: "Invalid URL",
+        preProcess: function() {
+            console.log("preProcess");
+        },
+        onSuccess: function() {
+            console.log("onSuccess");
+        },
+        onError: function() {
+            console.log("onError");
+        },
+        onComplete: function() {
+            console.log("onComplete");
+        }
+    });
+});
