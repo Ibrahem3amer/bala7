5070b5410000b9cab720595b8317fc7b13d7b5ca
ibrahem3amer <you@example.com>
2018-04-02 16:16:26 +0200

    Ad activation, deactivation, and changing lifetime were added.

--- ads/models.py
+++ ads/models.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.db import models
+from django.utils.timezone import now
 
 
 class BasicAd(models.Model):
@@ -22,6 +25,7 @@ class BasicAd(models.Model):
     )
     start_date = models.DateTimeField()
     creation_date = models.DateTimeField(auto_now_add=True)
+    termination_date = models.DateTimeField(null=True, blank=True)
     is_active = models.BooleanField(default=False)
     advertiser = models.ForeignKey(
         to='ads.Advertiser',
@@ -29,6 +33,35 @@ class BasicAd(models.Model):
         on_delete=models.CASCADE
     )
 
+    # Methods
+    def activate_ad(self):
+        """
+        Activates the add and set the deadline to the termination date.
+        :return: Void
+        """
+        self.start_date = now()
+        self.termination_date = self.start_date + timedelta(self.life_time)
+        self.is_active = True
+        self.save()
+
+    def deactivate_ad(self):
+        """
+        Deactivates a current ad and sets the termination date.
+        :return: Void
+        """
+        self.termination_date = now()
+        self.is_active = False
+        self.save()
+
+    def set_ad_lifetime(self, new_lifetime):
+        """
+        Sets new deadline to a current ad.
+        :param new_lifetime: A positive integer that represents the new lifetime of the add.
+        :return: Void
+        """
+        self.life_time = new_lifetime
+        self.termination_date = self.start_date + timedelta(new_lifetime)
+        self.save()
 
 class Advertiser(models.Model):
     """
--- ads/tests/test_models.py
+++ ads/tests/test_models.py
@@ -1,5 +1,8 @@
-from datetime import datetime
+from datetime import datetime, timedelta
+
 from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils.timezone import now
+
 from test_plus import TestCase
 
 from users.models import University, Faculty, Department
@@ -33,3 +36,88 @@ class BasicAdTest(TestCase):
         )
         # Test assertion
         self.assertEqual(BasicAd.objects.all().count(), 1)
+
+    def test_activate_ad(self):
+        """Assert that Ad activation method will handle the process of activation."""
+
+        # Test setup
+        advertiser = Advertiser.objects.create(
+            name='test_advertiser',
+            website='www.test.com'
+        )
+        ad = BasicAd.objects.create(
+            name='test_ad',
+            image=self.image,
+            department=self.dep,
+            life_time=3,
+            start_date=datetime.now(),
+            advertiser=advertiser
+        )
+        expected_termination_date = now() + timedelta(ad.life_time)
+
+        # Test body
+        ad.activate_ad()
+
+        # Test assertion
+        last_created_ad = BasicAd.objects.last()
+        self.assertTrue(last_created_ad.is_active)
+        self.assertIsNotNone(last_created_ad.termination_date)
+        self.assertEqual(last_created_ad.termination_date.date(), expected_termination_date.date())
+        self.assertGreaterEqual(last_created_ad.termination_date, last_created_ad.start_date)
+
+    def test_deactivate_ad(self):
+        """Assert that Ad deactivation method will disable the ad."""
+
+        # Test setup
+        advertiser = Advertiser.objects.create(
+            name='test_advertiser',
+            website='www.test.com'
+        )
+        ad = BasicAd.objects.create(
+            name='test_ad',
+            image=self.image,
+            department=self.dep,
+            life_time=3,
+            start_date=now()-timedelta(3),
+            advertiser=advertiser,
+            is_active=True,
+            termination_date=now()
+        )
+
+        # Test body
+        ad.deactivate_ad()
+
+        # Test assertion
+        last_created_ad = BasicAd.objects.last()
+        self.assertFalse(last_created_ad.is_active)
+        self.assertEqual(last_created_ad.termination_date.date(), now().date())
+
+    def test_change_ad_lifetime_active_ad(self):
+        """Assert that changing ad lifetime will affect its termination date."""
+
+        # Test setup
+        advertiser = Advertiser.objects.create(
+            name='test_advertiser',
+            website='www.test.com'
+        )
+        ad = BasicAd.objects.create(
+            name='test_ad',
+            image=self.image,
+            department=self.dep,
+            life_time=3,
+            start_date=datetime.now(),
+            advertiser=advertiser,
+            is_active=True
+        )
+        new_lifetime = 7
+        expected_termination_date = now() + timedelta(new_lifetime)
+
+        # Test body
+        ad.set_ad_lifetime(new_lifetime)
+
+        # Test assertion
+        last_created_ad = BasicAd.objects.last()
+        self.assertTrue(last_created_ad.is_active)
+        self.assertIsNotNone(last_created_ad.termination_date)
+        self.assertEqual(last_created_ad.termination_date.date(), expected_termination_date.date())
+        self.assertGreaterEqual(last_created_ad.termination_date, last_created_ad.start_date)
